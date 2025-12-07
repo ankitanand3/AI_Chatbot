b521ce6a758c732bfba17c0bd3542bb7ec5095a0
Ankit <ankitanand.kandra@gmail.com>
2025-12-07 09:04:31 -0500

    new features

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -49,7 +49,7 @@ if st.sidebar.button('New Chat'):
 
 st.sidebar.header('My Conversation')
 
-for thread_id in st.session_state['chat_threads']:
+for thread_id in st.session_state['chat_threads'][::-1]:
     if st.sidebar.button(str(thread_id)):
         st.session_state['thread_id'] = thread_id
         messages = load_conversation(thread_id)
